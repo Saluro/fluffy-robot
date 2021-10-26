262e32eac3104c4ede4a0ffdd3b989980bf6033a
Ilia <92857699+saluro@users.noreply.github.com>
2021-10-26 20:24:17 +0300

    z1 completed

--- part-1/z1/result.php
+++ part-1/z1/result.php
@@ -9,13 +9,17 @@ if (isset($_POST["form-submit"])) {
     $set1 = explode($sep1, $_POST["set-1"]);
     $set2 = explode($sep2, $_POST["set-2"]);
 
-//    try {
-//
-//    } catch (Exception $e) {
-//        $errText = "Вы некорректно указали одно из множеств";
-//    }
+    $sort1 = $_POST["set-1-sort"];
+    $sort2 = $_POST["set-2-sort"];
 
+    $set1_sorted = $set1;
+    $set2_sorted = $set2;
+    $sort1 ? sort($set1_sorted) : rsort($set1_sorted);
+    $sort2 ? sort($set2_sorted) : rsort($set2_sorted);
 
+    $set1_shuffled = $set1;
+    $set1_reverse = array_reverse($set1);
+    shuffle($set1_shuffled);
 }
 ?>
 
@@ -35,19 +39,19 @@ if (isset($_POST["form-submit"])) {
     <label class="results-label set-1">Первое множество: <?php echo $_POST["set-1"]; ?></label>
 <label class="results-label set-2">Второе множество: <?php echo $_POST["set-2"];; ?></label>
 <hr>
-<label class="results-label set-1-size-label">Количество элементов в первом множестве: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-2-size-label">Количество элементов во втором множестве: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-1-sorted-label">Отсортированное первое множество: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-2-sorted-label">Отсортированное второе множество: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-1-max-label">Максимальный элемент первого множества: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-2-max-label">Максимальный элемент второго множества: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-1-min-label">Минимальный элемент первого множества: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-2-min-label">Минимальный элемент второго множества: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-intersection-label">Пересечение множеств: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-difference-label">Разница множеств: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-1-reversed-label">Первое множество в обратном порядке: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-2-multiplication-label">Произведение элементов второго множества: <?php //переменная, рассчитанная выше?></label>
-<label class="results-label set-1-shuffled-label">Значения первого множества, перемешанные в случайном порядке: <?php //переменная, рассчитанная выше?></label>
+<label class="results-label set-1-size-label">Количество элементов в первом множестве: <?php echo count($set1); ?></label>
+<label class="results-label set-2-size-label">Количество элементов во втором множестве: <?php echo count($set2); ?></label>
+<label class="results-label set-1-sorted-label">Отсортированное первое множество: <?php echo implode(", ", $set1_sorted); ?></label>
+<label class="results-label set-2-sorted-label">Отсортированное второе множество: <?php echo implode(", ", $set2_sorted); ?></label>
+<label class="results-label set-1-max-label">Максимальный элемент первого множества: <?php echo max($set1); ?></label>
+<label class="results-label set-2-max-label">Максимальный элемент второго множества: <?php echo max($set2); ?></label>
+<label class="results-label set-1-min-label">Минимальный элемент первого множества: <?php echo min($set1); ?></label>
+<label class="results-label set-2-min-label">Минимальный элемент второго множества: <?php echo min($set2); ?></label>
+<label class="results-label set-intersection-label">Пересечение множеств: <?php echo implode(", ", array_intersect($set1, $set2)); ?></label>
+<label class="results-label set-difference-label">Разница множеств: <?php echo implode(", ", array_diff($set1, $set2)); ?></label>
+<label class="results-label set-1-reversed-label">Первое множество в обратном порядке: <?php echo implode(", ", $set1_reverse); ?></label>
+<label class="results-label set-2-multiplication-label">Произведение элементов второго множества: <?php echo array_product($set2); ?></label>
+<label class="results-label set-1-shuffled-label">Значения первого множества, перемешанные в случайном порядке: <?php echo implode(", ", $set1_shuffled); ?></label>
 </fieldset>
 
 </body>
