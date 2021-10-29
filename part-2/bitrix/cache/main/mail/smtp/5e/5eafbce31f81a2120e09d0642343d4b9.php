408c0028c5aec094bccb0668f864a9321e0134b7
Ilia <92857699+saluro@users.noreply.github.com>
2021-10-29 13:28:59 +0300

    part 2 initial commit

---
+++ part-2/bitrix/cache/main/mail/smtp/5e/5eafbce31f81a2120e09d0642343d4b9.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001635491725';
+$dateexpire = '001638083725';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";b:0;}';
+return true;
+?>
